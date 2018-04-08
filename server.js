d05bdb20e7808bfc41065cee2b2bb39f3bae3eed
Noel Serrato <Serrato@LGMBP.local>
2018-04-08 05:11:45 -0700

    MVP

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let bodyParser = require('body-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const path = require("path");
+let knex = require('./db/knex.js')
 
 
 const users = require('./routes/usersRoutes');
@@ -13,6 +14,8 @@ let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 server.listen(8080, function(){
   console.log("LISTENING ON PORT", 8080);
@@ -34,21 +37,42 @@ io.on('connection', socket => {
 
   })
 })//end of io.on()
+app.get("/channels",(req,res)=>{
+  console.log(req);
+  knex("channel")
+  .then((result)=>{
+    res.json(result);
+  })
+})
 
+
+app.post("/channel/create",(req,res)=>{
+  console.log(req);
+  knex("channel")
+  .insert({
+    host_id : 1,
+    friend_id:1 ,
+    is_active: true,
+    video_url: req.body.url
+  })
+  .then((result)=>{
+    console.log("success")
+    res.send(200);
+  })
+})
 app.use(logger("dev"));
 app.use(cors());
-app.use(bodyParser.json({extended: true}));
-app.use(bodyParser.urlencoded({extended: false}));
-//
-app.use(express.static(path.join(__dirname,"public")));
 
+
+// app.use(express.static(path.join(__dirname,"public")));
+//
 // require('./config/session.js')(app);
 //
 // app.set('view engine','ejs');
 //
-app.use('/users', users);
-app.use('/channel', channel);
-
+// app.use('/users', users);
+// app.use('/channel', channel);
+//
 // var routes = require('./config/routes.js');
 // routes(app);
 
